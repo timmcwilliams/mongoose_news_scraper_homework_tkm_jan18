85afec9e9ee74e68d9ef463f8626e0228d092949
tmac4403 <drive911@gmail.com>
2018-02-07 15:50:43 -0600

    rename and configure

---
+++ controllers/serverp.js
@@ -0,0 +1,31 @@
+// Parses our HTML and helps us find elements
+var cheerio = require("cheerio");
+// Makes HTTP request for HTML page
+var request = require("request");
+
+console.log("\n***********************************\n" +
+            "Grabbing every thread name and link\n" +
+            "from espn sports webdev board:" +
+            "\n***********************************\n");
+
+request("https://www.si.com/college-basketball", function(error, response, html) {
+
+  var $ = cheerio.load(html)
+
+  var results = [];
+
+    $("article.list-item").each(function(i, element){
+   
+    
+    var image =$(element).children(".media-img").attr("href");
+    var article = $(element).children().first().text();
+    console.log(article);
+  
+    results.push({
+      article: article,
+      image: image
+    });
+  });
+
+  console.log(results);
+});
